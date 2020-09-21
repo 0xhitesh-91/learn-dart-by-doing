dec58c47ab86d29ceba7832de39f354c1db3e09c
Hitesh Bhati <31737554+bhati-91@users.noreply.github.com>
2020-09-21 23:15:24 +0530

    getting started with the dart lang

---
+++ 01-hello-world.dart
@@ -0,0 +1,9 @@
+// hello-world program in dart
+
+main(){
+	print("hello world");
+}
+
+//main - it is entry point of our program from execution begin. it is special, required and top-level function.
+
+//print - print function is used to display/output the content between the double quotes to the screen.
---
+++ 02-dart-basic-syntax.dart
@@ -0,0 +1,28 @@
+/* 
+dart identifiers is a name given to program elements such as variables, array,
+class, function etc. an identifier is a sequence of letter, digits and underscores.
+
+- the first character must be an alphabet or can be underscore.
+- an identifier can not start with digit.
+- no special cheracter allowed except underscore and doller sign.
+- two sequential unserscore are not allowed.
+- keywords are not allowed as identifier name, as they already reserved.
+*/
+
+main(){
+	var name = "toothless";
+	var flight_time = 2.5;
+	
+	print("hey i am ${name} and flying from last ${flight_time} hours");
+}
+
+/*
+comments in dart
+
+- single line comment // comment
+- multi-line comment /* multiple lines goes here */
+
+string interpolation - ${variable}
+
+*/
+
